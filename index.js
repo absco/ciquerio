18a95b904f33ef45b52c2143ddf442f40396bf0a
abhaykumar-priocept <abhay.kumar@priocept.com>
2025-05-15 18:42:11 +0100

    Updates

--- index.js
+++ index.js
@@ -154,7 +154,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 var vite_config_default = defineConfig({
-  base: "/ciquerio",
+  base: "/ciquerio/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
